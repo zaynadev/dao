3b70467f48bdc461e8ff44f5f0e25eea2f507deb
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-03-18 23:07:47 +0100

    add deploy script for GovernanceToken contract

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+
+import { HardhatUserConfig } from "hardhat/config";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.18",
+// };
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  solidity: "0.8.18",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+};
+
+export default config;
